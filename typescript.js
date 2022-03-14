e42a3b1a03a18a316412998c9baa980c1aeb3597
Zorin <zorin1477@gmail.com>
2022-03-14 18:21:24 +0800

    fix: typescript

--- typescript.js
+++ typescript.js
@@ -3,15 +3,12 @@ const TSConfig = require('eslint-config-alloy/typescript');
 // Extract property 'plugins' and 'rules'
 const { plugins, rules } = TSConfig;
 
-// Omit unrelated rules
-const { rules: { 'react/sort-comp': ignored, ...TSRules } } = rules;
-
 module.exports = {
     plugins,
+    rules,
     parserOptions: {
         ecmaVersion: 12,
         parser: '@typescript-eslint/parser',
         sourceType: 'module',
     },
-    rules: TSRules,
 };
